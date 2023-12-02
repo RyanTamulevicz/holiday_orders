188f0a5f1d3bd45cba4e0bfa6ad11601cff986ee
Ryan Tamulevicz <ryantamulevicz14@gmail.com>
2023-12-02 12:23:48 -0500

    models

--- holiday/holiday/settings.py
+++ holiday/holiday/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_htmx'
+    'django_htmx',
+    'orderInput',
 ]
 
 MIDDLEWARE = [
