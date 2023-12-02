d4688574ea031c339f2fdd071c8b88db010815a1
Ryan Tamulevicz <ryantamulevicz14@gmail.com>
2023-12-02 12:57:58 -0500

    Remove unnecessary static file configurations

--- holiday/holiday/settings.py
+++ holiday/holiday/settings.py
@@ -119,11 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/holiday/static/',
-]
-STATIC_ROOT = "/var/www/holiday/static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
