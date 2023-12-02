3754c667476c9753397f2393ef239483f8862d76
Ryan Tamulevicz <ryantamulevicz14@gmail.com>
2023-12-02 17:24:09 -0500

    Update views and add input.html and searcher.html templates

--- searcher/views.py
+++ searcher/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 # make a search view
 def search(request):
-    return render(request, "index.html")
+    return render(request, "input.html")
