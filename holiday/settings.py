b5beb5b72553208ca2832654de5283712085bd5b
Ryan Tamulevicz <ryantamulevicz14@gmail.com>
2023-12-02 17:15:48 -0500

    Add whitenoise middleware to settings.py and update requirements.txt

--- holiday/settings.py
+++ holiday/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django_htmx.middleware.HtmxMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "holiday.urls"
